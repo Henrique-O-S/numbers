45f854fa49cf3aca98f1b0a9b8e9ed08a5437fb2
unknown <henri11silvafcc@gmail.com>
2021-11-26 15:53:29 +0000

    Class ListAggregatorTestSpock finnished

--- numbers/src/test/groovy/ListAggregatorTestSpock.groovy
+++ numbers/src/test/groovy/ListAggregatorTestSpock.groovy
@@ -9,6 +9,45 @@ class ListAggregatorTestSpock extends Specification {
         this.list = Arrays.asList(1,2,4,2,5)
     }
 
+    def "sum"() {
+
+        given:
+        def aggregator = new ListAggregator()
+
+
+        when:
+        def sum = aggregator.sum(list)
+
+        then:
+        sum == 14
+    }
+
+    def "max"(){
+
+        given:
+        def aggregator = new ListAggregator()
+
+
+        when:
+        def max = aggregator.max(list);
+
+        then:
+        max == 5
+    }
+
+    def "min" () {
+
+        given:
+        def aggregator = new ListAggregator()
+
+
+        when:
+        def min = aggregator.min(list);
+
+        then:
+        min == 1
+    }
+
     def "distinct" () {
 
         given:
@@ -16,23 +55,39 @@ class ListAggregatorTestSpock extends Specification {
 
 
         when:
-        def distinct = aggregator.distinct(list);
+        def distinct = aggregator.distinct(list)
 
         then:
         distinct == 4
     }
 
+    def "max_bug_7263"() {
+
+        given:
+        def bug_list = [-1, -4, -5]
+        def aggregator = new ListAggregator()
+
+
+        when:
+        def max = aggregator.max(bug_list);
+
+        then:
+        max == -1
+    }
+
     def "distinct_bug_8726"() {
 
         given:
+        def bug_list = [1, 2, 4, 2]
+        def aggregator = new ListAggregator()
         def deduplicator = Mock(GenericListDeduplicator)
         deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2)) >> Arrays.asList(1, 2, 4)
 
         when:
-        def result = deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2))
+        def distinct = aggregator.distinct(bug_list, deduplicator)
 
         then:
-        result == Arrays.asList(1,2,4)
+        distinct == 3
     }
 
 }
